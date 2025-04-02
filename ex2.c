f54f63056b7eac5728ecbf77a324afc87c89d931
andrelucas0982 <ribeiro.angeli111@gmail.com>
2025-04-01 21:26:25 -0300

    Add files via upload

---
+++ ex2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	
+	int A, Soma = 0;
+	
+	do{
+		puts("Digite o numero do dado: ");
+		scanf("%d", &A);
+		
+		
+		
+		if(A % 2 == 1){
+			break;
+		} else 
+		Soma += A;
+		
+	} while(1);
+	
+	printf("A soma dos numeros do Dado apresentado e igual a: %d", Soma);
+	
+	return 0;
+}
